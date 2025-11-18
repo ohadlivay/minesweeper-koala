3ad0e9265f04b52581872870ef317642b1bb4e8d
Ohad Livay <156086151+ohadlivay@users.noreply.github.com>
2025-11-18 20:48:56 +0200

    setting allowed imports

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -1,4 +1,13 @@
+/* Asserted imports for this class:
+model.GameSession
+view.BoardLayout
+ */
+
 package main.java.controller;
+
+import main.java.model.GameSession;
+import main.java.view.BoardLayout;
+
 
 public class BoardController {
 }
