b428aca7faa887ee40ff8cfd910b1030f5ca739b
Ohad Livay <156086151+ohadlivay@users.noreply.github.com>
2025-11-19 17:02:04 +0200

    setting up board contemplating access modifiers

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,4 +1,22 @@
+/* Asserted imports for this class:
+java.util.Random
+ */
+
 package main.java.model;
+import java.util.Random;
 
 public class Board {
+
+    private final int PK;
+    private int minesLeft;
+    private Tile[][] tiles;
+    private static final Random RANDOM = new Random();
+
+    private Board(){
+        this.PK = RANDOM.nextInt(99999999);
+
+        this.tiles = new Tile[10][10];
+    }
+
+
 }
