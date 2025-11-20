cbc333f7e8accf0dcf294da21d126d8e84eed5ec
T-Sap <tomsapir2722@gmail.com>
2025-11-20 21:06:45 +0200

    Creation of the partial SysData

--- src/main/java/model/SysData.java
+++ src/main/java/model/SysData.java
@@ -1,4 +1,56 @@
 package main.java.model;
 
-public class SysData {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SysData
+{
+    //Singleton instance
+    private static SysData instance = null;
+    //List of game data
+    private final List<GameData> games;
+
+    //Private constructor
+    private SysData()
+    {
+        games = new ArrayList<>();
+    }
+
+    //Getter for the singleton instance
+    public static SysData getInstance()
+    {
+        if (instance == null)
+            instance = new SysData();
+        return instance;
+    }
+
+    //Method to add a game to the list
+    public void addGame(GameData game)
+    {
+        games.add(game);
+    }
+
+    //Method to get a game from the list
+    public GameData getGame(int index)
+    {
+        return games.get(index);
+    }
+
+    //Method to get the number of games in the list
+    public int getNumberOfGames()
+    {
+        return games.size();
+    }
+
+    //Method to clear the list of games
+    public void clearGames()
+    {
+        games.clear();
+    }
+
+    //Method to get a copy of the game list
+    public List<GameData> getGames()
+    {
+        return new ArrayList<>(games);
+    }
 }
