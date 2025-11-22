71e05c6c2b4f6dab536971a9456cf716b9bb8dc8
TTaliR <talir9400@gmail.com>
2025-11-22 16:33:10 +0200

    Adjusted buttons and added exit button

--- src/main/java/view/StartScreen.java
+++ src/main/java/view/StartScreen.java
@@ -5,17 +5,18 @@ import javax.swing.*;
 public class StartScreen {
     private JPanel mainPanel;
     private JLabel startScreenLabel;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JButton startGameBtn;
+    private JButton gameHistoryBtn;
+    private JButton mngQuestionsBtn;
+    private JButton exitBtn;
     private final NavigationController nav;
 
     public StartScreen(NavigationController nav) {
         this.nav = nav;
 
-        button1.addActionListener(e -> nav.goToGame());
-        button2.addActionListener(e -> nav.goToHistory());
-        button3.addActionListener(e -> nav.goToQuestionManager());
+        startGameBtn.addActionListener(e -> nav.goToGame());
+        gameHistoryBtn.addActionListener(e -> nav.goToHistory());
+        mngQuestionsBtn.addActionListener(e -> nav.goToQuestionManager());
     }
 
     public JPanel getMainPanel() {
