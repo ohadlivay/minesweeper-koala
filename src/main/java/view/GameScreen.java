89a07ed207aa45e643565930af152a090dd9abb6
TTaliR <talir9400@gmail.com>
2025-11-23 14:50:51 +0200

    created GameScreen main frame

--- src/main/java/view/GameScreen.java
+++ src/main/java/view/GameScreen.java
@@ -5,6 +5,9 @@ import main.java.controller.NavigationController;
 import javax.swing.*;
 
 public class GameScreen extends JPanel {
+    private JPanel gameScreen;
+    private JPanel board1Placeholder;
+
     public GameScreen(NavigationController navigationController) {
     }
 }
