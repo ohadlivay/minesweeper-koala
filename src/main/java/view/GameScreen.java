9d4fb659fef90576dd710de47327c936dd2ae72d
TTaliR <talir9400@gmail.com>
2025-11-23 16:34:25 +0200

    added player names

--- src/main/java/view/GameScreen.java
+++ src/main/java/view/GameScreen.java
@@ -7,8 +7,12 @@ import javax.swing.*;
 public class GameScreen extends JPanel {
     private JPanel gameScreen;
     private JPanel board1Placeholder;
-    private JPanel board2Placeholder;
     private JButton exitBtn;
+    private JPanel wrapperPanel;
+    private JLabel player1Label;
+    private JPanel wrapperPanel2;
+    private JPanel board2Placeholder;
+    private JLabel player2Label;
 
     public GameScreen(NavigationController navigationController) {
     }
