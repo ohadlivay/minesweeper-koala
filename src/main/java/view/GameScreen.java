bcc73d5b67a7fb49a9974acccf9d161e2e60ebb1
TTaliR <talir9400@gmail.com>
2025-11-23 15:15:53 +0200

    set board placeholders

--- src/main/java/view/GameScreen.java
+++ src/main/java/view/GameScreen.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class GameScreen extends JPanel {
     private JPanel gameScreen;
     private JPanel board1Placeholder;
+    private JPanel board2Placeholder;
 
     public GameScreen(NavigationController navigationController) {
     }
