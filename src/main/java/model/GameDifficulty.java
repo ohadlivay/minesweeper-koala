5341cb26cf5d0b035e40ef6522a95284b850d57e
T-Sap <tomsapir2722@gmail.com>
2025-11-25 11:58:20 +0200

    added surprise tiles distribution to GameDifficulty enum

--- src/main/java/model/GameDifficulty.java
+++ src/main/java/model/GameDifficulty.java
@@ -5,27 +5,37 @@ package main.java.model;
    need to add more info like activation cost or penalties which are specific to difficulty.
 */
 public enum GameDifficulty {
-    EASY(9, 9, 10, 5),
-    MEDIUM(13, 13, 40, 8),
-    HARD(16, 16, 99, 12);
+    EASY(9, 9, 10,6,2, 5),
+    MEDIUM(13, 13, 40,7,3, 8),
+    HARD(16, 16, 99,11,4, 12);
 
     //even though rows=cols in all current  cases, we want to be prepared to support a feature where this is not the case.
     private final int rows;
     private final int cols;
 
     private final int mineCount;
+    private final int questionCount;
+    private final int surpriseCount;
 
     private final int activationCost;
 //
-    private GameDifficulty(int rows, int cols, int mineCount, int activationCost) {
+    private GameDifficulty(int rows, int cols, int mineCount,int questionCount, int surpriseCount, int activationCost) {
         this.rows = rows;
         this.cols = cols;
         this.mineCount = mineCount;
         this.activationCost = activationCost;
+        this.questionCount = questionCount;
+        this.surpriseCount = surpriseCount;
     }
 
     public int getRows()      { return rows; }
     public int getCols()      { return cols; }
     public int getMineCount() { return mineCount; }
     public int getActivationCost() { return activationCost; }
+    public int getQuestionCount() {
+        return questionCount;
+    }
+    public int getSurpriseCount() {
+        return surpriseCount;
+    }
 }
