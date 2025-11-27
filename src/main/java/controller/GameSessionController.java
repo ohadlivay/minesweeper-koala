faaf24804b3e5f76d31a86d4453d49c6e977ffd3
liranBecher <bliran2@gmail.com>
2025-11-27 16:12:03 +0200

    Update GameSessionController.java

--- src/main/java/controller/GameSessionController.java
+++ src/main/java/controller/GameSessionController.java
@@ -5,5 +5,23 @@ model.GameSession
 
 package main.java.controller;
 
+import main.java.model.GameSession;
+
 public class GameSessionController {
+
+    private static GameSessionController controller;
+
+    private GameSessionController() {
+        GameSessionController.controller = new GameSessionController();
+    }
+
+    public static GameSessionController getinstance() {
+        if (GameSessionController.controller == null) {
+            GameSessionController.controller = new GameSessionController();
+        }
+        return GameSessionController.controller;
+    }
+
+
 }
+
