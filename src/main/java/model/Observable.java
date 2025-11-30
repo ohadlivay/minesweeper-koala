b147a1b16504456c61986c9d263fdfdc35b73502
Ohad Livay <156086151+ohadlivay@users.noreply.github.com>
2025-11-30 19:12:48 +0200

    deleted unused classes

--- src/main/java/model/Observable.java
+++
@@ -1,9 +0,0 @@
-package main.java.model;
-/* this is an Observer design pattern. its like client-server, where the server is the Observer.
-the Observer gets notified when the Subject (the client) performs any state change.
-
-we will use this pattern in usecases such as: boards give updates to gamesession.
- */
-public interface Observable {
-    void update(int newValue);
-}
