ae0dc744a997ff2a29c2f93f7b34163d3e601aaf
Tali Ratner <talir9400@gmail.com>
2025-12-01 12:17:01 +0200

    configured JUnit test test

--- unittests/test.java
+++ unittests/test.java
@@ -3,7 +3,6 @@ import org.junit.Test;
 public class test {
     @Test
     public void dummyTest() {
-        // This is a placeholder test method.
-        // Actual test methods should be implemented here.
+        assert(true);
     }
 }
