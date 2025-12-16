f6aceb8887c9f578f6e05735be7104e247c0467b
TTaliR <talir9400@gmail.com>
2025-12-16 20:47:34 +0200

    added lib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import com.formdev.flatlaf.themes.FlatMacLightLaf;
 import main.java.controller.NavigationController;
 import main.java.model.BoardGenerator;
 import main.java.model.GameDifficulty;
-import main.java.test.Testable;
 import main.java.util.GameDataCSVManager;
 import main.java.util.QuestionCSVManager;
 
