52e6c6f232d9b2f094a0fef25eeafd316c7ea951
liranBecher <bliran2@gmail.com>
2025-12-20 12:47:49 +0200

    Refactor DummyTileView for testing and update TileViewTest assertions

--- unittests/DummyTileView.java
+++ unittests/DummyTileView.java
@@ -3,14 +3,16 @@ import main.java.view.TileView;
 
 import java.awt.*;
 
+// Dummy TileView class for testing purposes
 public class DummyTileView extends TileView{
     boolean recolorCalled = false;
     boolean uncolorCalled = false;
 
-    public DummyTileView(Tile tile, int dynamicSize) {
-        super(tile, dynamicSize, Color.black);
+    public DummyTileView(Tile tile) {
+        super(tile, 40, Color.black); // Using arbitrary size and color for testing
     }
 
+    // overrides to make methods testable
     @Override
     protected void recolorTile() {
         recolorCalled = true;
--- unittests/TileViewTest.java
+++ unittests/TileViewTest.java
@@ -5,19 +5,22 @@ import main.java.model.Tile;
 
 public class TileViewTest {
 
+    // Test if the setTileTurn method calls the correct methods:
     // Check if the recolorTile and uncolorTile methods are called correctly
-    // Test if the setTileTurn method calls the correct methods
-    // RecolorTile should be called when the tile is turned on
-    // UncolorTile should be called when the tile is turned off
+    //  - RecolorTile should be called when the tile is turned on
+    //  - UncolorTile should be called when the tile is turned off
     @Test
     public void setTileTurn_true_callsRecolorTile() {
 
         Tile dummyTile = new Tile();
-        DummyTileView tileView = new DummyTileView(dummyTile, 40);
+        DummyTileView tileView = new DummyTileView(dummyTile);
 
         tileView.setTileTurn(true);
 
+        // Assert that the recolorTile method was called
         Assert.assertTrue(tileView.recolorCalled);
+
+        // Assert that the uncolorTile method was not called
         Assert.assertFalse(tileView.uncolorCalled);
     }
 
@@ -25,11 +28,14 @@ public class TileViewTest {
     @Test
     public void setTileTurn_false_callsUncolorTile() {
         Tile dummyTile = new Tile();
-        DummyTileView tileView = new DummyTileView(dummyTile, 40);
+        DummyTileView tileView = new DummyTileView(dummyTile);
 
         tileView.setTileTurn(false);
 
+        // Assert that the uncolorTile method was called
         Assert.assertFalse(tileView.recolorCalled);
+
+        // Assert that the recolorTile method was not called
         Assert.assertTrue(tileView.uncolorCalled);
     }
 
