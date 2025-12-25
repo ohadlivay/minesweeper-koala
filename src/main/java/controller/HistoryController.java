f772f10fe9b938d7a198e39794f379a5838a591d
Tom Sapir <tomsapir2722@gmail.com>
2025-12-25 23:27:15 +0200

    Game History controller ready for only presenting the games

---
+++ src/main/java/controller/HistoryController.java
@@ -0,0 +1,33 @@
+package main.java.controller;
+
+import main.java.model.GameData;
+import main.java.model.SysData;
+import main.java.view.GameHistoryScreen;
+
+import java.util.List;
+
+public class HistoryController {
+    private GameHistoryScreen view;
+    private SysData sysData;
+
+    private static HistoryController instance;
+
+    public static HistoryController getInstance()
+    {
+        if(instance == null){
+            instance = new HistoryController();
+        }
+        return instance;
+    }
+
+    public void setView(GameHistoryScreen view) {
+        this.view = view;
+    }
+
+    public void refreshGamesList()
+    {
+        if (view == null) return;
+        List<GameData> games = SysData.getInstance().getGames();
+        //view.populateTable(games);//A method for you to implement
+    }
+}
