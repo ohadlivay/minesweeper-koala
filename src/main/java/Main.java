420ce28d467be30f5c8ea25ec715e59bd4f19388
liranBecher <bliran2@gmail.com>
2025-12-27 21:52:35 +0200

    slight window size adjustment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,7 +58,7 @@ public class Main {
 
                 JFrame frame = new JFrame("Koala Minesweeper");
                 frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-                frame.setSize(1200, 680);
+                frame.setSize(1200, 690);
                 frame.setResizable(false);
                 frame.addWindowListener(new WindowAdapter() {
                     @Override
